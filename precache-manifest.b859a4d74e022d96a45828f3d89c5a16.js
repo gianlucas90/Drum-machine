d4af43d6e046a9a8a7ddc1ff642b227b391a98b9
Gianluca <gianlucazin1990@gmail.com>
2020-09-23 23:29:45 +0200

    Updates

---
+++ precache-manifest.b859a4d74e022d96a45828f3d89c5a16.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "042fca4c863358c2a6f2c43a34544809",
+    "url": "/Drum-machine/index.html"
+  },
+  {
+    "revision": "5fa52b9cb4a334b7af88",
+    "url": "/Drum-machine/static/css/main.1f5d1b58.chunk.css"
+  },
+  {
+    "revision": "f21e3229be1c79dac8fe",
+    "url": "/Drum-machine/static/js/2.3ace00df.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Drum-machine/static/js/2.3ace00df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5fa52b9cb4a334b7af88",
+    "url": "/Drum-machine/static/js/main.78e959dc.chunk.js"
+  },
+  {
+    "revision": "e80790fa3c434aa25bfc",
+    "url": "/Drum-machine/static/js/runtime-main.95518db7.js"
+  },
+  {
+    "revision": "abe03961c7511cc994b64bcebc93b0f2",
+    "url": "/Drum-machine/static/media/Cev_H2.abe03961.mp3"
+  },
+  {
+    "revision": "5c9f80876a2f7c9c7dd4dba4a1784f95",
+    "url": "/Drum-machine/static/media/Dsc_Oh.5c9f8087.mp3"
+  },
+  {
+    "revision": "b1cfea05e9aeb27be5b041e40c1ca80f",
+    "url": "/Drum-machine/static/media/Heater-1.b1cfea05.mp3"
+  },
+  {
+    "revision": "0d219666c08f6037c2f4c6ccfa8c71bb",
+    "url": "/Drum-machine/static/media/Heater-2.0d219666.mp3"
+  },
+  {
+    "revision": "632e877262affcec87e77ef0a409969c",
+    "url": "/Drum-machine/static/media/Heater-3.632e8772.mp3"
+  },
+  {
+    "revision": "d9de8a500f2556463e3a3784274f1847",
+    "url": "/Drum-machine/static/media/Heater-4_1.d9de8a50.mp3"
+  },
+  {
+    "revision": "3062daca6b46c54601c994dcdda8ef6b",
+    "url": "/Drum-machine/static/media/Heater-6.3062daca.mp3"
+  },
+  {
+    "revision": "200b4bd34ad53416203739f8ab241e3b",
+    "url": "/Drum-machine/static/media/Kick_n_Hat.200b4bd3.mp3"
+  },
+  {
+    "revision": "09fda25678eb5321a1b3d456bda527e1",
+    "url": "/Drum-machine/static/media/RP4_KICK_1.09fda256.mp3"
+  }
+]);
